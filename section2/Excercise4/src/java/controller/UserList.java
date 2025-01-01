aa9d7c525aca417913117def068641273e0f567d
PhuPham70 <phupdhe181976@fpt.edu.vn>
2025-01-01 14:51:54 +0700

    Section2-the first

---
+++ section2/Excercise4/src/java/controller/UserList.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author pp384
+ */
+@WebServlet(name="UserList", urlPatterns={"/user/list"})
+public class UserList extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet UserList</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<table>");
+            out.println("<thead><tr><th>STT</th><th>FullName</th><th>Email</th></tr></thead>");
+            out.println("<tbody>");
+            out.println("<tr><td>1</td><td>John</td><td>john@gmail.com</td></tr>");
+            out.println("<tr><td>2</td><td>Tony</td><td>tony@gmail.com</td></tr>");          
+            out.println("</table>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    } 
+
+    
+     
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
